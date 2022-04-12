97a46c8d29a09a1b8b779d9dcd92afa563db1476
randolf alegre <dolf.alegre1989@gmail.com>
2022-04-12 22:13:35 +0800

    PPM-9

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const transactionService = require("../services/transaction");
 const makeTransactionController = require("./transactionController");
+const makeUserController = require("./userController");
 
 const transactionController = makeTransactionController({ transactionService });
+const userController = makeUserController({ transactionService });
 
 module.exports = Object.freeze({
   transactionController,
+  userController,
 });
--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const transactionRouter = require("./routes/transactionRouter");
-
+const userRouter = require("./routes/userRouter");
+const whitelistDomain = ["http://griph.xyz"];
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ origin: "http://griph.xyz", credentials: true }));
+app.use(cors({ origin: whitelistDomain, credentials: true }));
 
 app.use("/api/transactions", transactionRouter);
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port::${PORT}`);
