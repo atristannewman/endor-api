5baba27b535419cc7340f572e55f5955c62bed01
Tristan <93885277+atristannewman@users.noreply.github.com>
2022-06-29 17:38:12 -0400

    Revert "Ppm 143 implement database migration using sequelize previously flexsin branch"

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,8 +3,6 @@ const userRouter = express.Router();
 const { userController } = require("../controllers");
 const requestHandler = require("../requestHandler");
 
-userRouter.get("/", requestHandler(userController.getUsers));
 userRouter.get("/getProfile", requestHandler(userController.getProfile));
-userRouter.post("/", requestHandler(userController.createUser));
 
 module.exports = userRouter;
