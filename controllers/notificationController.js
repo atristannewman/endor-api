b88648caa658fa764a09c0b9367f9b74b1883f86
grim2888 <nvatar7@gmail.com>
2022-07-30 08:11:21 -0500

    added apns key

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -9,42 +9,42 @@ exports.setNotificationToken = function(req, res, next) {
     })
 }
 
-exports.createNotification = function(req, res, next) {
-    var user_id = req.params.user_id;
-    User.findById(user_id, function(err, user) {
-      if (err) { return next(err) }
-      var token = user.apn_token;
+// exports.createNotification = function(req, res, next) {
+//     var user_id = req.params.user_id;
+//     User.findById(user_id, function(err, user) {
+//       if (err) { return next(err) }
+//       var token = user.apn_token;
   
-      var options = {
-        token: {
-            // cert: __dirname + '/cert.pem',
-            key: __dirname + '/key.pem',
-            teamId: "FlockApp"
-        }
+//       var options = {
+//         token: {
+//             // cert: __dirname + '/cert.pem',
+//             key: __dirname + '/key.pem',
+//             teamId: "FlockApp"
+//         }
         // ,
         // proxy: {
         //   host: "192.168.10.92",
         //   port: 8080
         // }
         // production: false
-      };
+      // };
 
-      var apnConnection = new apn.Connection(options);
+      // var apnConnection = new apn.Connection(options);
   
-      var myDevice = new apn.Device(token);
+  //     var myDevice = new apn.Device(token);
   
-      var note = new apn.Notification();
+  //     var note = new apn.Notification();
   
-      note.expiry = Math.floor(Date.now() / 1000) + 3600;
-      note.badge = 3;
-      note.sound = "ping.aiff";
-      note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-      note.payload = {'messageFrom': 'Caroline'};
-      apnConnection.pushNotification(note, myDevice);
+  //     note.expiry = Math.floor(Date.now() / 1000) + 3600;
+  //     note.badge = 3;
+  //     note.sound = "ping.aiff";
+  //     note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+  //     note.payload = {'messageFrom': 'Caroline'};
+  //     apnConnection.pushNotification(note, myDevice);
   
-      return res.json({success: "true"});
-    });
-  }
+  //     return res.json({success: "true"});
+  //   });
+  // }
 
 module.exports = ({ DB }) => {
 
@@ -79,7 +79,7 @@ module.exports = ({ DB }) => {
 
         console.log(`test device token ${deviceToken}`);
 
-        apnProvider.send(notification, deviceToken, sound).then( (result) => {
+        apnProvider.send(notification, deviceToken).then( (result) => {
           // see documentation for an explanation of result
             console.log(`send notification request response, ${result}`);
           });
