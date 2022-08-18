d93686dad4a5d2f081a5545f5ba86cb833a0972f
Waqar Yazdani <wy@softwarealliance.dk>
2022-08-18 20:18:47 +0500

    Validation fixed

--- validation/user.js
+++ validation/user.js
@@ -8,9 +8,9 @@ const schema = Joi.object({
   hostRating:Joi.number().required(),
   profileImageUrl:Joi.string().optional(),
   location: Joi.object({
-    latitude:Joi.number().greater(0).required().allow('null',null),
-    longitude:Joi.number().greater(0).required().allow('null',null)
-  }).required(),
+    latitude:Joi.number().greater(0).required().allow('null',null,''),
+    longitude:Joi.number().greater(0).required().allow('null',null,'')
+  }).optional().allow('','null',null),
 });
 
 exports.validate = (args) => {
--- validation/userUpdate.js
+++ validation/userUpdate.js
@@ -8,9 +8,9 @@ const schema = Joi.object({
   profileImageUrl: Joi.string().optional(),
   notificationPreferences: Joi.object().optional(),
   location: Joi.object({
-    latitude:Joi.number().greater(0).required().allow('null',null),
-    longitude:Joi.number().greater(0).required().allow('null',null)
-  }).required(),
+    latitude:Joi.number().greater(0).required().allow('null',null,''),
+    longitude:Joi.number().greater(0).required().allow('null',null,'')
+  }).optional().allow('','null',null),
 });
 
 exports.userUpdateValidate = (args) => {
