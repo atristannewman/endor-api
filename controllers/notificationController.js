77b6011c239b0c2d4de03d0533ec26a2496fe951
grim2888 <tbnewman2@gmail.com>
2022-09-16 09:30:20 -0500

    removed debug code

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -188,18 +188,6 @@ module.exports = ({ DB }) => {
               notificationPreferences: potentialGuest.notificationPreferences, 
               deviceToken: potentialGuest.deviceToken 
             });
-
-            // TODO: Why is this happening VVV ? 
-            // notifiedUserArray.push({ 
-            //   address: potentialGuest.address, 
-            //   location: potentialGuest.location, 
-            //   notificationPreferences: potentialGuest.notificationPreferences, 
-            //   deviceToken: potentialGuest.deviceToken 
-            // });
-
-            // Answer
-            // we only need the deviceToken other fields were added for debugging purpose
-            // As wanted to compare location and check against which user address etc
           } else {
             console.log("Not enough potential attendees within potential guests (of origin users) preferred radius, guest wallet address: ", potentialGuest.address);
           }
