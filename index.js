206b6dd2ab93e31f37af1a2127a1d21fda5138b1
grim2888 <tbnewman2@gmail.com>
2022-10-21 16:43:36 +0200

    registering and logging in through auth0

--- index.js
+++ index.js
@@ -7,20 +7,53 @@ const userRouter = require("./routes/userRouter");
 const vendorRouter = require("./routes/vendorRouter");
 const hangoutsRouter = require("./routes/hangoutsRouter");
 const notificationsRouter = require("./routes/notificationsRouter");
+const { auth, requiresAuth } = require('express-openid-connect');
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || '23.90.200.170';
+const PORT = process.env.PORT || '5432';
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded());
 
+// ----- Routes ------ //
 app.use("/api/transactions", transactionRouter);
 app.use("/api/users", userRouter);
 app.use("/api/vendors", vendorRouter);
 app.use("/api/hangouts", hangoutsRouter);
 app.use("/api/notifications", notificationsRouter);
+
+// ----- from Auth0 start ------ //
+const auth0Configuration = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  // callback: process.env.AUTH0_CALLBACK_URL,
+  // redirectUri: process.env.AUTH0_CALLBACK_URL,
+  secret: process.env.AUTH0_SECRET,
+  // onRedirectCallback: "http://localhost:5432/"
+};
+
+// The `auth` router attaches /login, /logout
+// and /callback routes to the baseURL
+app.use(auth(auth0Configuration));
+
+// req.oidc.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+  )
+});
+
+// The /profile route will show the user profile as JSON
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+// ----- from Auth0 end ----- //
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port::${PORT}`);
