15925fa91e8cce50d9a65fe601901f1d8d9fefca
grim2888 <tbnewman2@gmail.com>
2022-10-22 20:12:24 +0200

    changed name of table in migration file

--- migrations/20221021155554-add-auth0Id-column.js
+++ migrations/20221021155554-add-auth0Id-column.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "auth0Id", {
+    await queryInterface.addColumn("user", "auth0Id", {
       type: Sequelize.STRING,
       defaultValue: null,
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn("users", "auth0Id");
+    await queryInterface.removeColumn("user", "auth0Id");
   }
 };
