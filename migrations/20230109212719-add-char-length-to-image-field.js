7cfde1983269dd3b399a615f246471f7aedad518
Tristan Newman <tbnewman2@gmail.com>
2023-01-09 15:40:48 -0600

    removed space from migration file

--- migrations/20230109212719-add-char-length-to-image-field.js
+++ migrations/20230109212719-add-char-length-to-image-field.js
@@ -2,24 +2,15 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn(
-      'user', 
-        'profileImageUrl', 
-          {
+    await queryInterface.changeColumn('user', 'profileImageUrl',{
             type: Sequelize.STRING(2049),
             allowNull: true
-          }
-    )
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn(
-      'user', 
-        'profileImageUrl', 
-          {
-            type: Sequelize.STRING,
-            allowNull: true
-          }
-    )
-  }
+    await queryInterface.changeColumn('user', 'profileImageUrl',{
+      type: Sequelize.STRING,
+      allowNull: true
+})
 };
