cc6498503370fcb88e1537d704cbc525b77c610b
Tristan Newman <tbnewman2@gmail.com>
2023-04-10 17:24:57 -0500

    patch for package error getting one month earlier than the current Date when calling Date()

--- utils/dateUtil.js
+++ utils/dateUtil.js
@@ -1,6 +1,6 @@
 function getCurrDateString() {
   const currDatetime = new Date();
-  const fullDateString = `${currDatetime.getFullYear()}-${currDatetime.getMonth()}-${currDatetime.getDate()}`;
+  const fullDateString = `${currDatetime.getFullYear()}-${currDatetime.getMonth() + 1}-${currDatetime.getDate()}`;
   const fullTimeString = `${currDatetime.getHours()}:${currDatetime.getMinutes()}:${currDatetime.getSeconds()}`;
   const timezoneOffsetString = `${currDatetime.getTimezoneOffset()}`;
   const fullDatetimeString = `${fullDateString} ${fullTimeString} +${timezoneOffsetString}`;
